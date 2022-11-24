df510c3425b35eb80ee41cbc55d4ae2cf596f3fd
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-11-24 15:10:08 +0000

    [Add] 各種コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'homes#top' 
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+  end
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :customers, only: [:show, :edit, :update]
+    get '/customers/confirm' => 'customers#confirm'
+    patch '/customers/withdrow' => 'customers#withdrow'
+    resources :orders, only: [:new, :index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/complete' => 'orders#complete'
+    resources :adresses, except: [:new, :show]
+  end
 end
