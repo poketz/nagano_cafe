b560d677c623d3c48ccd5ff1967b739d01528b6a
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-12-04 10:05:28 +0000

    [Add] 顧客トップページの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { not_received: 0, confirming: 1, making: 2, preparing: 3, shipped: 4 } 
 end
