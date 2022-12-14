79bdaa92912fad9235949e94bf6c618c00a14d81
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-12-14 15:19:19 +0000

    [Add] 注文情報入力・確認画面の作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,12 +5,25 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     @address.save!
+    redirect_to addresses_path
+  end
+  
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+  
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
     redirect_to addresses_path
   end
 
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,24 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+
+  def confirm
+    @cart_items = current_customer.cart_items.all
+    @total_price = 0
+    @order = Order.new(order_params)
+    select_address = params[:order][:select_address]
+    case select_address
+    when '0'
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    when '1'
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
   end
 
   def complete
@@ -9,5 +28,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
