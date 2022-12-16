60c0542b3a601d5b30992fd93a307895c43029d1
hiroki <ec2-user@ip-172-31-40-253.ap-northeast-1.compute.internal>
2022-12-16 05:50:50 +0000

    [Add] 注文一覧・詳細表示機能の実装

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -43,9 +43,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_customer.orders.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
 
   private
